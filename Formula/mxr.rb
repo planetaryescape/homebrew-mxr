f3ab9bd47b9e10bd11b4cdb5aab8aaae81235090
Bhekani Khumalo <bhekanik@gmail.com>
2026-03-20 15:23:20 +0000

    mxr 0.1.0
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Formula/mxr.rb
+++ Formula/mxr.rb
@@ -1,13 +1,13 @@
 class Mxr < Formula
   desc "Local-first terminal email client"
   homepage "https://github.com/planetaryescape/mxr"
-  version "__VERSION__"
+  version "0.1.0"
   license "MIT OR Apache-2.0"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/planetaryescape/mxr/releases/download/v#{version}/mxr-v#{version}-macos-aarch64.tar.gz"
-      sha256 "__SHA256_MACOS_AARCH64__"
+      url "https://github.com/planetaryescape/mxr/releases/download/v0.1.0/mxr-v0.1.0-macos-aarch64.tar.gz"
+      sha256 "5bd08fafa279dab5886c3abf21ff407202cf974020942e0d8f0f3773aa031b4e"
     else
       url "https://github.com/planetaryescape/mxr/releases/download/v#{version}/mxr-v#{version}-macos-x86_64.tar.gz"
       sha256 "__SHA256_MACOS_X86_64__"
